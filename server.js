d7f2b040a2574d06077dddb14472c764c869b2d9
ibrahim <ibrahimjlidi23@gmail.com>
2026-08-28 11:25:43 +0200

    fix: correct quizattempt route import casing for Linux

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/inscriptions", require("./routes/inscriptionRoutes"));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 app.use("/api/questions", require("./routes/questionRoutes"));
-app.use("/api/quizattempts", require("./routes/quizAttemptRoutes"));
+app.use("/api/quizattempts", require("./routes/quizattemptRoutes"));
 app.use("/api/recommendations", require("./routes/recommendationRoutes"));
 app.use("/api/performancemetrics", require("./routes/performanceMetricRoutes"));
 app.use("/api/auditlogs", require("./routes/auditLogRoutes"));
